5f93b7789a4c71d05db5284e8f63b0dd12a1a769
Cassio M. M. Pereira <cassiomartini@gmail.com>
2017-03-31 10:38:23 -0300

    max drawdown

--- ch03/sharpe-ratio.R
+++ ch03/sharpe-ratio.R
@@ -29,6 +29,7 @@ sharpeRatio = function(prices, annumRiskFreeRate = 0.04, tradingDays = 252) {
     sqrt(tradingDays) * mean(excessret) / sd(excessret) # sharpe ratio
 }
 
+
 hedgedSharpe = function(prices1, prices2, annumRiskFreeRate = 0.04, tradingDays = 252) {
 
     if (nrow(prices1) != nrow(prices2)) {
@@ -43,5 +44,40 @@ hedgedSharpe = function(prices1, prices2, annumRiskFreeRate = 0.04, tradingDays 
     sqrt(tradingDays) * mean(netReturns) / sd(netReturns) # sharpe ratio
 }
 
-srLong = sharpeRatio(IGE) # this is a long strategy, of buying on 2001 and staying with it until 2007.
-srLongShortNeutral = hedgedSharpe(IGE, SPY) # this is a long short (neutral) strategy.
+#srLong = sharpeRatio(IGE) # this is a long strategy, of buying on 2001 and staying with it until 2007.
+#srLongShortNeutral = hedgedSharpe(IGE, SPY) # this is a long short (neutral) strategy.
+
+computeMaxDrawdownAndDuration = function(cumret) {
+    N = length(cumret)
+
+    highwatermark = rep(0, N)
+    drawdown = rep(0, N)
+    drawdownduration = rep(0, N)
+
+    for (i in 2:N) {
+        highwatermark[i] = max(highwatermark[i - 1], cumret[i])
+        drawdown[i] = (1 + cumret[i]) / (1 + highwatermark[i]) - 1 # drawdown each day
+        if (drawdown[i] == 0) {
+            drawdownduration[i] = 0
+        } else {
+            drawdownduration[i] = drawdownduration[i - 1] + 1
+        }
+    }
+
+    c(min(drawdown), max(drawdownduration))
+}
+
+maxDrawdown = function(prices1, prices2) {
+    # calculate the high watermark - maximum cumulative return of
+    # the strategy up to that time (the close of each day).
+    # first compute the daily returns:
+    adjclose1 = as.numeric(prices1[,6]); adjclose2 = as.numeric(prices2[,6])
+    dailyret1 = computeDailyReturn(adjclose1); dailyret2 = computeDailyReturn(adjclose2)
+
+    netReturns = (dailyret1 - dailyret2) / 2.0
+    cumulativeCompoundedReturn = cumprod(1 + netReturns) - 1
+    plot(cumulativeCompoundedReturn, type='l')
+    computeMaxDrawdownAndDuration(cumulativeCompoundedReturn)
+}
+
+mds = maxDrawdown(IGE, SPY)
